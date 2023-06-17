8d6e63f0e1462d1eb59c90277a01ff12b25cd0af
Jamesl3483 <jlukas6249@gmail.com>
2023-06-17 15:16:28 -0700

    fix name

--- hume.py
+++
@@ -1,7 +0,0 @@
-from hume import HumeBatchClient
-from hume.models.config import FaceConfig
-
-client = HumeBatchClient("QUANHZQQ2VRBSixAhGfNvsDGCWuGu5TnUk03l5S2lukw32sI")
-urls = ["https://iep.utm.edu/wp-content/media/hume-bust.jpg"]
-config = FaceConfig()
-job = client.submit_job(urls, [config])
